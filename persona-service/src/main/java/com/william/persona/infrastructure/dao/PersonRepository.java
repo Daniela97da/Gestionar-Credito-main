687bfdc61f5a70f5c5e84d3eda3831af6c97f0e0
Daniela97da <daleja.0797@gmail.com>
2024-08-20 19:27:24 -0500

    Update PersonRepository.java

--- persona-service/src/main/java/com/william/persona/infrastructure/dao/PersonRepository.java
+++ persona-service/src/main/java/com/william/persona/infrastructure/dao/PersonRepository.java
@@ -1,6 +1,4 @@
-package com.william.persona.infrastructure.dao;
 
-import com.william.persona.domain.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
